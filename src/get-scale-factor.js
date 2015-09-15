dc9ef0dced3054d02631911c5f723af3f8ad701c
Jessica Stokes <hello@jessicastokes.net>
2015-09-15 15:17:00 +1000

    Restore helpful comment about why we use Math.min

--- src/get-scale-factor.js
+++ src/get-scale-factor.js
@@ -1,7 +1,10 @@
 import getScreenWidth from './get-screen-width';
 
 export default function getScaleFactor() {
-  var outerWidth = Math.min(getScreenWidth(), window.outerWidth || Infinity);
+  // `window.outerWidth` is `0` on iOS devices running iOS 8.0 and higher, and
+  // astronomically large (read: also incorrect) on iOS devices running iOS 7.x
+  // and below, so we'll use `screenWidth` instead in those cases.
+  const outerWidth = Math.min(window.outerWidth || Infinity, getScreenWidth());
 
   return window.innerWidth / outerWidth;
 }
